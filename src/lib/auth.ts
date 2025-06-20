4dd2ac391f30fbd5c65d85d95cee715cb2a0accb
Anton5555 <antonmartinezq@gmail.com>
2025-06-20 11:24:35 -0300

    chore: import existing prisma instance

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { twoFactor, organization } from "better-auth/plugins";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prisma } from "./data";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
